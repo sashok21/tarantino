390b1a0b3ffbdc684a2c81cb91b9319f3e901366
VladyslavPats <vladyslav.v.pats@ukd.edu.ua>
2025-04-07 11:05:25 +0300

    перегляд усіх існуючих опитувань

--- tarantino/polls/views.py
+++ tarantino/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Question
 
-# Create your views here.
+def question_list(request):
+    questions = Question.objects.all()
+    context = {'questions': questions}
+    return render(request, 'question_list.html', context)
